1a67f4f9407fb6ff9dddf34272177eef081616c6
youfeed <11247005@qq.com>
2024-03-17 07:31:11 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 
 <template>
   <div>
-    <button @click="onAccept">收听 单连接</button>
-    <img ref="image" src="https://th.bing.com/th?id=ODLS.3dc6f5f0-f66b-4880-893d-62d29cba6f62&w=32&h=32&qlt=93&pcl=fffffa&o=6&pid=1.2" style="background: #fff;width: 120px;"/>
+    <button @click="onAccept" style="position: fixed;right: 10px;top: 10px;">收听 缓冲池({{ buffer.length }})</button>
+    <!-- <img ref="image" src="https://th.bing.com/th?id=ODLS.3dc6f5f0-f66b-4880-893d-62d29cba6f62&w=32&h=32&qlt=93&pcl=fffffa&o=6&pid=1.2" style="background: #fff;width: 120px;"/> -->
 
     <div class="phone">
-        <div v-for="(item,index) in images" :key="index">
-          <img :src="item.src" alt="" class="item">
+        <div v-for="(item) in images" :key="item.uuid">
+          <YouCanvas :uuid="item.uuid" :task="item.task" :key="item.uuid"></YouCanvas>
+          <!-- <img :ref="onRef(item)" :src="item.src" alt="" class="image"> -->
+          <!-- <canvas  :ref="el=>{canvasRefs[item.uuid]=el}"  :data-uuid="item.uuid" class="canvas"></canvas> -->
+          <!-- <canvas :ref="el=>onRef(el,item)" :data-uuid="item.uuid" class="canvas"></canvas> -->
         </div>
     </div>
     <!-- <canvas ref="canvas"></canvas>
@@ -18,6 +21,7 @@
   </div>
 </template>
 <script setup>
+import YouCanvas from './components/YouCanvas.vue'
 // import JMuxer  from 'jmuxer'
 // import WSAvcPlayer from 'ws-avc-player'
 // import './assets/YUVCanvas'
@@ -26,6 +30,7 @@
 // import './assets/Player'
 // import './assets/stream'
 import { ref, onMounted, reactive, toRefs } from 'vue'
+const buffeRef = ref([])
 
 const state = reactive({
   player:null,image:null,video:null,canvas:null,ctx:null,wss:null,play:null,jmuxer:null,videoCanvas:null,
@@ -35,10 +40,15 @@ const state = reactive({
   accept:`wss://wss1.zzxxl.cn/admin`,
   // 图片池
   images:[],
+  // 缓冲池
+  buffer:buffeRef
 })
 // 
 onMounted(()=>{
   let {player,accept} = state;
+  // console.log(itemRefs)
+  // onDraw();
+  // state.ctx = state.canvas.getContext('2d')
   // const jmuxer = new JMuxer({
   //     node: player,
   //     mode: 'video',
@@ -81,13 +91,103 @@ onMounted(()=>{
   //   // 
   // }
 })
+// 动态布局 
+const onRef = (el,item)=>{
+  if(item.refed != el.firstElementChild){
+    item.refed = el.firstElementChild;
+    if(el.firstElementChild.nodeName == 'CANVAS'){
+      item.ctx = el.firstElementChild.getContext('2d');
+    }
+    console.log(111111111,el,item)
+  }
+  // console.log(222222222,el,item)
+  // if(item && index && !item.ref){
+  //   console.log(222222222)
+  //   if(el.nodeName == 'CANVAS'){
+  //     item.ctx = el.getContext('2d');
+  //   }
+  // }
+  // if (!item[index]) {
+  //   item[index].ref = el;
+  // }
+  // item.ref = el;
+  // console.log(1,el,item,index)
+}
+// const onRef = (item)=>(el)=>{
+//   item.ref = el;
+//   if(el.nodeName == 'CANVAS'){
+//     item.ctx = el.getContext('2d');
+//   }
+//   console.dir(item.ref)
+// }
+// 
+const arrayBufferToBase64 = (buffer)=>{
+  // console.log(buffer)
+    var binary = '';
+    var bytes = new Uint8Array(buffer);
+    // var bytes = buffer;
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+  }
 // 绘制画布
 const onDraw = ()=>{
-  let {canvas,video,ctx} = state,{width,height} = canvas;
-  ctx.clearRect(0,0,width,height)
-  ctx.drawImage(video,0,0,width,height)
-  requestAnimationFrame(onDraw);
-}
+  let {uuid,buff} = state.buffer.shift() || {}
+  // 直接渲染图片模式
+  // if(uuid){
+  //   let {ref} = onCanvas(uuid)
+  //   var url= arrayBufferToBase64(buff);
+  //   // console.log(url,buff)
+  //   ref.src='data:image/jpeg;base64,'+url;
+  // }
+  // 画布渲染模式
+  // console.dir(100000000,uuid)
+  // if(uuid){
+  //   let {refed,ctx} = find = onCanvas(uuid);
+  //   if(refed == undefined){
+  //     let canvas = canvasRefs.value[uuid]
+  //     find.refed = canvas
+  //     find.ctx = canvas.getContext('2d')
+  //     console.log(888888888888888,find,uuid,canvas)
+  //   }
+  //   if(refed){
+  //     console.log(100,refed.width,refed.height,ctx)
+  //     // let {width,height} = ref
+  //     let NImg = new Image();
+  //     NImg.onload = function (e) {
+  //       console.log(NImg)
+  //       ctx.drawImage(NImg, 0, 0,375 * 4,667 * 4,0,0,375,667);
+  //       URL.revokeObjectURL(NImg.src)
+  //     }
+  //     let blob = new Blob([buff],{type:'image/jpg'})
+  //     NImg.src = window.URL.createObjectURL(blob)
+  //     console.log('画布渲染模式',uuid)
+  //   }
+  // }
+  // let dpr = 4;
+  //   let NImg = new Image();
+  //   NImg.onload = function (e) {
+  //     console.log(999,this,img)
+  //     ref.src = this.src
+  //   //   console.log(uuid,ref.width,ref.height)
+  //   //   ctx.clearRect(0, 0, ref.width, ref.height);
+  //   //   ctx.drawImage(img, 0, 0,375 * dpr,667 * dpr,0,0,300,300);//,375 * dpr,667 * dpr,0,0,ref.width,ref.height
+  //   //   // ctx.scale(devicePixelRatio, devicePixelRatio);
+  //   //   // image.srcObject = img;
+  //   //   // find.src = img.src
+  //   //   URL.revokeObjectURL(img.src)
+  //   }
+  //   NImg.onerror = function (e) {
+  //     console.log(233,e)
+  //   }
+  //   NImg.src = window.URL.createObjectURL(blob)
+  // let {canvas,video,ctx} = state,{width,height} = canvas;
+  // ctx.clearRect(0,0,width,height)
+  // ctx.drawImage(video,0,0,width,height)
+  // requestAnimationFrame(onDraw);
+};
 // 截图上传
 const onCapture = ()=>{
   const {canvas,wss} = state
@@ -133,62 +233,63 @@ const onAccept = ()=>{
     wss.send(JSON.stringify([{"type":"setAutoGetImageTimerGap","gapTime":"1.0"}]))
   }
   wss.onmessage = ({data})=>{
-    onImage(data)
+    if(typeof data == 'object'){
+      onImage(data)
+    }else{
+      console.log('接收:',data)
+    }
     // jmuxer.feed({
     //   video:new Uint8Array(data)
     // })
-    console.log('接收:',data)
   }
   wss.onclose = ()=>{
     console.log('接收:断开连接')
   }
 }
-// 展示图片
+// 动态生成
+const onCanvas = (uuid)=>{
+  let {images,ctx} = state
+  let find = images.find(item=>item.uuid === uuid)
+  if(find == undefined){
+    const taskRef = ref([])
+    images.push({
+      uuid:uuid,
+      task:taskRef,
+      src:'',
+      ref:null
+    });
+    find = images.find(item=>item.uuid === uuid)
+  }
+  // console.log(canvasRefs)
+  return find
+}
 // 前8字符
 const onImage = (data)=>{
-  let {image,images} = state
+  let {images,buffer} = state
   let slicedBuffer = data.slice(0,32)
   let decoder = new TextDecoder('utf-8');
   let uuid = decoder.decode(new Uint8Array(slicedBuffer),{stream: true}).split('$').join(''); // 
-
-  console.log(slicedBuffer,uuid,image)
   let buff = data.slice(32,-1)
-  let blob = new Blob([buff],{type:'image/jpg'})
-  let src = URL.createObjectURL(blob)
-  let find = images.find(item=>item.uuid === uuid)
-  if(find == undefined){
-    images.push({
-      uuid:uuid,
-      src:src
-    });
-  }else{
-    find.src = src
-    if(uuid == 139){
-      image.src = src
-    }
+  if(uuid){
+    let {task} = onCanvas(uuid);
+    // console.log('task',task)
+    task.push({uuid:uuid,buff:buff})
+    // buffer.push({uuid:uuid,blob:blob})
   }
-  image.srcObject = blob;
-  // if(images[uuid]){
-  //   images[uuid].src = src
-  // }else{
-  //   images[uuid] = {
-  //     src:src
-  //   }
-  // }
-  // console.log(src)
-  
-  // image.onload = ()=>{
-  //   URL.revokeObjectURL(image.src)
-  // }
-  // image.src = imageUrl
 }
-const { images,image,video,canvas,ctx,player,videoCanvas } = toRefs(state)
+const { images,image,video,canvas,ctx,player,videoCanvas,buffer } = toRefs(state)
 </script>
 <style scoped>
-canvas,video{
-  max-width: 160px;
+/* .canvas{
+  width: 100%;
+  height: 100%;
+} */
+.canvas,.image{
+  background: #f3efef;
+  /* width: 100%; */
   /* width: 376px; */
   /* height: 666px; */
+  /* aspect-ratio: 9 / 19; */
   border: 1px solid #000;
 }
 video{
@@ -200,7 +301,5 @@ video{
   grid-template-columns: repeat(6, 200px);
   /* align-items: center; */
 }
-.item{
-  max-width: 100%;
-}
+
 </style>
---
+++ src/components/YouCanvas.vue
@@ -0,0 +1,70 @@
+<template>
+  <div style="border: 1px solid #000;">
+    <canvas class="canvas" ref="canvas"></canvas>
+    <img class="image" ref="image">
+  </div>
+</template>
+
+<script setup>
+import { onMounted,reactive, toRefs } from 'vue';
+const props = defineProps({
+  task: Array,
+});
+const state = reactive({
+  image:null,
+  canvas:null,
+  ctx:null,
+  tasks:[]
+})
+onMounted(()=>{
+  state.tasks = props.task;
+  state.ctx = state.canvas.getContext('2d');
+  console.log(1000000000000000000,state.tasks,props.task)
+  onDraw();
+})
+const onDraw = ()=>{
+  let {image,canvas,ctx,tasks} = state
+  let {uuid,buff} = tasks.shift() || {}
+  // 直接渲染图片模式
+  // if(uuid){
+  //   let {ref} = onCanvas(uuid)
+  //   var url= arrayBufferToBase64(buff);
+  //   // console.log(url,buff)
+  //   ref.src='data:image/jpeg;base64,'+url;
+  // }
+  // 画布渲染模式
+  // console.dir(100000000,uuid)
+  if(uuid){
+    let {width,height} = canvas
+    console.log(100,canvas.width,canvas.height,ctx)
+    let NImg = new Image();
+    NImg.onload = function (e) {
+      // console.log(NImg)
+      ctx.drawImage(NImg,0,0);// , 0, 0,375 * 4,667 * 4,0,0,width,height
+      image.src = NImg.src
+      image.onload = function (e) {
+        console.log('imageimageimage',uuid)
+      }
+      URL.revokeObjectURL(NImg.src)
+      requestAnimationFrame(onDraw);
+    }
+    let blob = new Blob([buff],{type:'image/jpg'})
+    NImg.src = window.URL.createObjectURL(blob)
+    console.log('画布渲染模式',uuid)
+  }else{
+    requestAnimationFrame(onDraw);
+  }
+  
+};
+const {canvas,image} = toRefs(state)
+</script>
+
+<style scoped>
+.canvas{
+  max-width: 100%;
+  
+}
+.image{
+  max-width: 100%;
+}
+</style>
